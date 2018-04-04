4db2f98323ea39704719cbc6d56dff65b00e6173
Sayan Banerjee <banerjee97sayan@gmail.com>
2018-04-04 22:54:20 +0530

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int n;
+struct priority
+{
+	float arrival_t,burst_t,ct,waiting_t,turnaround_t;
+	int priority,status;
+	char process_name;
+}[10];
+struct RR
+{
+	float arrival_t,burst_t,ct,waiting_t,turnaround_t;
+	int priority,status;
+	char process_name;
+}
+void main()
+{
+	
+}
