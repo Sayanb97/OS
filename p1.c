1c1e5eccf68a218e70d63886e304c57183b321f0
Sayan Banerjee <banerjee97sayan@gmail.com>
2018-04-05 23:14:15 +0530

    Add files via upload

--- p1.c
+++ p1.c
@@ -1,19 +1,34 @@
 #include<stdio.h>
 #include<conio.h>
 int n;
-struct priority
+struct Priority
 {
-	float arrival_t,burst_t,ct,waiting_t,turnaround_t;
+	int arrival_t,burst_t,ct,waiting_t,turnaround_t;
 	int priority,status;
 	char process_name;
-}[10];
-struct RR
+}priority_queue[10];
+struct Round
 {
-	float arrival_t,burst_t,ct,waiting_t,turnaround_t;
-	int priority,status;
+	int arrival_t,burst_t,ct,waiting_t,turnaround_t;
+	int priority,status,TQ ;
 	char process_name;
-}
+}RR[10];
 void main()
 {
-	
+	int burst=0,time=0,largest;
+	char c;
+	printf("Enter no. of processes ");
+	scanf("%d",n);
+	for(int i=0,c='A';i<n;i++,c++)
+	{
+		priority_queue[i].process_name=c;
+		printf("Enter details for Process[%c]",c);
+		printf("Enter arrival time ");
+		scanf("%d",priority_queue[i].arrival_t);
+		printf("Enter burst time ");
+		scanf("%d",priority_queue[i].burst_t);
+		printf("Enter priority ");
+		scanf("%d",priority_queue[i].priority);
+		priority_queue.status=0;
+	}	
 }
